8d6a470cd9cab59e9fca1eb355f9017bb306ae64
Grindpa <anyanwufrancischu@yahoo.com>
2025-12-07 13:12:37 +0100

    done with 70% of everything

--- organizer.py
+++ organizer.py
@@ -29,12 +29,13 @@ def get_tasks():
     payload = {
         "model": "accounts/fireworks/models/gpt-oss-120b",
         "messages": messages,
-        "max_tokens": 1000
+        "max_tokens": 1300
     }
     url = "https://api.fireworks.ai/inference/v1/chat/completions"
 
     r = httpx.post(url, headers=headers, json=payload)
     json_data = r.json()
+    # return json_data
     data = json_data["choices"][0]["message"]["content"]
     if "done" in data:
         tasks = json.loads(data)
@@ -69,4 +70,4 @@ def get_tasks():
     else:
         return "invalid response"
     
-print(get_tasks())
+# print(get_tasks())
--- static/js/one-decision.js
+++ static/js/one-decision.js
@@ -1,21 +1,102 @@
 document.addEventListener("DOMContentLoaded", async () => {
     if (document.body.id === "index") {
         let form = document.querySelector("main form");
-        let checks = document.querySelectorAll("main form .check");
         let count = document.querySelector("form .setCont .count span");
+        let choices_1 = document.querySelector("form .choices_1");
+        let choices_2 = document.querySelector("form .choices_2");
+        let loader = document.querySelector(".load");
+        let infoImg = document.querySelector(".indexH img");
+        let infoCont = document.querySelector(".infoCont");
+        let info = infoCont.querySelector(".infoCont .info");
+        
+        infoCont.style.display = "none";
+        
+        choices_1.style.display = "none";
+        choices_2.style.display = "none";
+
+        infoImg.addEventListener("click", () => {
+            if (infoCont.style.display === "none") {
+                infoCont.style.display = "block";
+            }
+            else {
+                infoCont.style.display = "none";
+            }
+        });
+        infoCont.addEventListener("click", () => {
+            if (infoCont.style.display === "block") {
+                infoCont.style.display = "none";
+            }
+            else {
+                infoCont.style.display = "block";
+            }
+        });
+        info.addEventListener("click", (e) => {
+            e.stopPropagation();
+        });
+
 
         async function get_tasks() {
             try {
                 r = await fetch("/api/tasks");
                 data = await r.json();
-                console.log(data.msg);
+                let tasks = data.msg;
+                console.log(tasks)
+
+                for (let i=0; i < 10; i++) {
+                    let choice = document.createElement("div");
+                    let check = document.createElement("input");
+                    let label = document.createElement("label");
+
+                    choice.classList.add("choice");
+                    check.classList.add("check");
+                    check.id=`check ${i+1}`;
+                    check.value=tasks[i];
+                    check.name=`check ${i+1}`;
+                    check.type="checkbox";
+
+                    label.setAttribute("for", check.id);
+                    label.textContent = tasks[i];
+
+                    // console.log(label.textContent);
+                    choices_1.appendChild(choice);
+                    choice.appendChild(check);
+                    choice.appendChild(label);
+                }
+                for (let i=10; i < 20; i++) {
+                    let choice = document.createElement("div");
+                    let check = document.createElement("input");
+                    let label = document.createElement("label");
+
+                    choice.classList.add("choice");
+                    check.classList.add("check");
+                    check.id=`check ${i+1}`;
+                    check.value=tasks[i];
+                    check.name=`check ${i+1}`;
+                    check.type="checkbox";
+
+                    label.setAttribute("for", check.id);
+                    label.textContent = tasks[i];
+
+                    // console.log(label.textContent);
+                    choices_2.appendChild(choice);
+                    choice.appendChild(check);
+                    choice.appendChild(label);
+
+                    choices_1.style.display = "grid";
+                    choices_2.style.display = "grid";
+                    loader.style.display = "none";
+                }
             }
             catch(error) {
                 console.log({error: error});
+                setTimeout(() => alert("request is taking longer than expected, try refreshing this page"), 10000);
             }
             
         }
         await get_tasks()
+
+        let checks = document.querySelectorAll("form .check");
+        console.log(checks)
 
         form.addEventListener("submit", (e) => {
             e.preventDefault();
